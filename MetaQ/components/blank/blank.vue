78ad4d103ec82215c0830dfc708a9ddc72deb368
ChenYX24 <90974567+ChenYX24@users.noreply.github.com>
2023-05-19 13:38:16 +0800

    分享问卷

---
+++ MetaQ/components/blank/blank.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="Blank">
+		<view class="title">
+			{{title}}
+		</view>
+		<textarea class="content" maxlength="900"></textarea>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"blank",
+		props:{
+		},
+		data() {
+			return {
+				title:'this is a title'
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+	.content{
+		background-color: rgba(255, 255, 255,1);
+	}
+</style>
