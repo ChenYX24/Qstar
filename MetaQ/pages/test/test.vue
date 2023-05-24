42db5dc02308d7fa9476b9f2d46c6e78563e983b
ChenYX24 <90974567+ChenYX24@users.noreply.github.com>
2023-05-24 20:41:21 +0800

    配置vuex

--- MetaQ/pages/test/test.vue
+++ MetaQ/pages/test/test.vue
@@ -1,66 +1,53 @@
 <template>  
-  <view>  
-    <view class="table-row first">  
-      <text class="table-cell">选项</text>  
-      <text class="table-cell">比例</text>  
-      <text class="table-cell">数量</text>  
-    </view>  
-    <view class="table-row" v-for="(item, index) in pieData" :key="index">  
-      <text class="table-cell">{{ item.name }}</text>  
-      <text class="table-cell">{{ calcPercentage(item.value) }}</text>  
-      <text class="table-cell">{{ item.value }}</text>  
-    </view>  
-    <view class="table-row last">  
-      <text class="table-cell">总人数</text>  
-      <text class="table-cell"></text>  
-      <text class="table-cell">{{ totalValue }}</text>  
-    </view>  
+  <view class="container">  
+    <text class="title">当前计数：{{ count }}</text>  
+    <button @click="increment">增加</button>  
+    <button @click="decrement">减少</button>  
+    <button @click="incrementAsync">异步增加</button>  
+    <text class="title">双倍计数：{{ doubleCount }}</text>  
   </view>  
 </template>  
   
 <script>  
 export default {  
-  data() {  
-    return {  
-      pieData: [  
-        { value: 1048, name: 'Search Engine'},  
-        { value: 735, name: 'Direct'},  
-        { value: 580, name: 'Email'},  
-        { value: 484, name: 'Union Ads'},  
-        { value: 300, name: 'Video Ads'},  
-        { value: 300, name: 'Vdeo'},  
-        { value: 300, name: 'Vieo'},  
-        { value: 300, name: 'Vido'},  
-        { value: 300, name: 'Vide'},  
-      ],  
-    };  
-  },  
   computed: {  
-    totalValue() {  
-      return this.pieData.reduce((acc, item) => acc + item.value, 0);  
+    count() {  
+      return this.$store.state.count;  
+    },  
+    doubleCount() {  
+      return this.$store.getters.doubleCount;  
     },  
   },  
   methods: {  
-    calcPercentage(value) {  
-      return ((value / this.totalValue) * 100).toFixed(2) + '%';  
+    increment() {  
+      this.$store.commit('increment');  
+    },  
+    decrement() {  
+      this.$store.commit('decrement');  
+    },  
+    incrementAsync() {  
+      this.$store.dispatch('incrementAsync');  
     },  
   },  
 };  
 </script>  
   
-<style>  
-.table-row {  
-  display: flex;  
-  justify-content: space-between;  
-  padding: 10px 0;  
-  border-bottom: 1px solid #eee;  
-  background: rgba(250, 251, 255, 1);
-}
-.first,.last{
-	background: rgba(240, 244, 253, 1);
-}
-.table-cell {  
-  flex: 1;  
+<style scoped>  
+.title {  
+  font-size: 32rpx;  
   text-align: center;  
+  margin-bottom: 16rpx;  
+}  
+  
+button {  
+  display: block;  
+  width: 300rpx;  
+  height: 60rpx;  
+  line-height: 60rpx;  
+  font-size: 30rpx;  
+  text-align: center;  
+  margin: 0 auto 16rpx auto;  
+  background-color: #007aff;  
+  color: white;  
 }  
 </style>  
