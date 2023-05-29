1cd25a15ce47dca6ca0ebe723149b104b6760fa8
ChenYX24 <90974567+ChenYX24@users.noreply.github.com>
2023-05-29 22:29:37 +0800

    111

--- MetaQ/components/add/add.vue
+++ MetaQ/components/add/add.vue
@@ -1,123 +1,9 @@
-<!-- <template>
-	<view class="addContainer" :class="{show:isShow}">
-		<view class="imgAndText">
-			<image src="https://tuanpet-cyx.oss-cn-guangzhou.aliyuncs.com/static/add/addNotebook.png" mode="widthFix" class="addImg imgNote" @tap="addNote"></image>
-			<text class="Text notbookT">新建备忘事件</text>
-		</view>
-		<view class="imgAndText">
-			<image src="https://tuanpet-cyx.oss-cn-guangzhou.aliyuncs.com/static/add/addActivityNote.png" mode="widthFix" class="addImg"  @tap="ActivityThoughtCreate"></image>
-			<text class="Text notbookT">新建活动笔记</text>
-		</view>
-		<view class="imgAndText">
-			<image src="https://tuanpet-cyx.oss-cn-guangzhou.aliyuncs.com/static/add/addPetIDCard.png" mode="widthFix" class="addImg" @tap="IDCreate"></image>
-			<text class="Text">新建宠物身份证</text>
-		</view>
-		<view class="imgAndText">
-			<image src="https://tuanpet-cyx.oss-cn-guangzhou.aliyuncs.com/static/add/addPetIcon.png" mode="widthFix" class="addImg face imgNote"></image>
-			<text class="Text notbookT">新建萌宠表情</text>
-		</view>
-	</view>
-</template>
-
-<script>
-	export default {
-		name:"add",
-		data() {
-			return {
-				
-			};
-		},
-		props: {
-			isShow: {
-				type: Boolean,
-				default: false,
-			},
-		},
-		methods: {
-			IDCreate() {
-				this.$emit('updateAdd');
-				uni.navigateTo({
-					 url: `/pages/IDCreate/IDCreate/IDCreate`,
-				})
-			},
-			addNote(){
-				
-				const item = ['', '', '', '', '']; // 初始化为五个空字符串的数组
-				const index = -1; // 初始化为 -1
-				
-				wx.setStorageSync('myData', { item, index });
-				this.$emit('updateAdd');
-				uni.navigateTo({
-					 url: `/pages/notebook/notebookForm`,
-				})
-			},
-			ActivityThoughtCreate() {
-				this.$emit('updateAdd');
-				uni.navigateTo({
-					 url: `/pages/activity/ActivityThoughtCreate`,
-				})
-			}
-		},
-	}
-</script>
-
-<style lang="less">
-	.addContainer{
-	z-index: 998;
-	background-color: rgba(255, 255, 255, 0.3);
-	position: fixed;
-	display: grid;
-	grid-template-columns: repeat(2, 200px);
-	grid-template-rows: repeat(2, 200px);
-
-	bottom: -100%;
-	left: 0;
-	right: 0;
-	height: 100%;		
-	// animation: slide-up 0.5s ease-in-out forwards;
-	transition: bottom 0.5s ease-in-out;
-	align-items: center;
-	justify-items: center;
-	justify-content: center;
-	align-content: center;
-		.imgAndText{
-			box-shadow: 5px 7px 8px -1px rgba(239,219,208,0.5);
-			height: 40vw;
-			width: 40vw;
-			background-color: #fcceb5;
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			align-items: center;
-			margin: 3vw;
-			border-radius: 10%;
-			.addImg{
-				height: 15vw;
-				width: 15vw;
-			}
-			.addImg.imgNote{
-				margin-bottom: 0.5rem;
-			}
-			.addImg.face{
-				height: 13vw;
-				width: 13vw;
-			}
-			.Text
-			{
-				color: #ffffff;
-				font-family: SimHei;
-			}
-		}
-	}
-	.addContainer.show{
-		bottom: 0;
-	}
-</style> -->
 <template>
 	<view class="addContainer" :class="{show:isShow}">
 		<view class="questionnire_page" id='questionnire_page'>
-			<view class="input_class" id='input_class_block'>
-				<view class="textarea_border">
+			<view class="input_class2" id='input_class2_block'>
+				
+				<view class="textarea_border2">
 					<textarea maxlength="900"
 							  id="title"
 							  placeholder="请输入问卷标题"
@@ -126,7 +12,7 @@
 							  disabled
 						></textarea>
 				</view>
-				<view class="textarea_border">
+				<view class="textarea_border2">
 							  <textarea maxlength="900"
 							  id="descripition"
 							  placeholder="请输入问卷简介"
@@ -144,14 +30,6 @@
 				:is="chooseComponent(index)">
 				</component>
 			</view>
-			
-
-<!-- 			<view class="">
-				<view class="submit" @click="submitSuccess">
-					提交
-				</view>
-			</view> -->
-			
 
 		</view>
 		
@@ -173,15 +51,25 @@
 			huadongtiaoAnswer,
 			duoxuanAnswer
 		},
-		props: {
-			isShow: {
-				type: Boolean,
-				default: false,
+		onLoad: function(options) {
+			if(options.mode){
+				this.mode=options.mode
+			}
+		},
+		props:{
+			all_content:{
+				type:Array,
+				default:[]
 			},
+			isShow:{
+				type:Boolean,
+				default:false
+			}
 		},
 		data() {
 			return {
 				answer:[],
+				mode:0,
 				componentName:['danxuanAnswer','duoxuanAnswer',
 				'tiankongAnswer','huadongtiaoAnswer'],
 				title:'关于c10居住学生学校住宿感受的的调研',
@@ -210,7 +98,7 @@
 						{
 							title:"滑动条",
 							type:'3',
-							choice:['m','n','b'],
+							choice:[10,'非常差',1000,'非常好',0],
 						}],
 			}
 		},
@@ -223,16 +111,20 @@
 				var temp=parseInt(this.all_content[index].type);
 				return this.componentName[temp];
 			},
-			// submitSuccess(){
+			submitSuccess(){
+				this.$refs.questionAnswerRef.forEach(childComponent => {
+						this.answer.push(childComponent.answer);
+				});
+				console.log(this.answer)
 				
-				
-			// }
+			}
 		}
 
 	};
 </script>
 
 <style scoped lang="less">
+	
 *{
 	margin: 0;
 	box-sizing: border-box;
@@ -275,25 +167,25 @@
 }
 
 
-.textarea_border{
+.textarea_border2{
 	border: 3px solid rgba(255, 255, 255, 1);
 	display: flex;
 }
-.input_class .textarea_border:first-child {
+.input_class2 .textarea_border2:first-child {
 	margin-top: 4vh;
 	width: 80vw;
 	display: flex;
 	align-items: center;
 	padding: 2% 5% 2% 5%;
 }
-.input_class .textarea_border:nth-child(2){
+.input_class2 .textarea_border2:nth-child(2){
 		margin-top: 2vh;
 		width: 80vw;
 		display: flex;
 	padding: 2% 5% 2% 5%;
 }
 
-.textarea_border{
+.textarea_border2{
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
@@ -301,7 +193,7 @@
 }
 
 
-.textarea_border:first-child textarea {
+.textarea_border2:first-child textarea {
 	height: 3vh;
 	font-size: 24px;
 	color: rgba(0, 0, 0, 1);
@@ -314,7 +206,7 @@
 	width: 100%;
 }
 
-.textarea_border:last-child textarea {
+.textarea_border2:last-child textarea {
 	// margin-top: 2vh;
 	height: 3vh;
 	line-height: 27.44px;
@@ -322,19 +214,5 @@
 	font-size: 20px;
 	width: 100%;
 	// line-height: 8vh;
-}
-	
-.submit{
-	margin-top: 20px;
-	margin-bottom: 50px;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	width: 70vw;
-	height:10vh;
-	border: 1px solid #9c3ced;
-	border-radius: 10px;
-	background: linear-gradient(58deg, #b094f8 0%, #e4daf1 38.89%, #f2f2f2 67.78%, #e4b8f5 100%);
-	
 }
 </style>
